d642065ba971ba5ddb78cd3fb66c5e163722824b
Karthikn96 <82644968+Karthikn96@users.noreply.github.com>
2021-04-18 04:24:52 +0530

    Update summa.py

--- summa.py
+++ summa.py
@@ -3,17 +3,6 @@ import streamlit.components.v1 as stc
 import pandas as pd
 import numpy as np
 import string
-import nltk
-from nltk import pos_tag
-from nltk.corpus import stopwords
-from nltk.tokenize import WhitespaceTokenizer
-from nltk.stem import WordNetLemmatizer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from gensim.test.utils import common_texts
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.corpus import wordnet
-import streamlit.components.v1 as stc
 
 
 st.title("Question 1 Part 2")
