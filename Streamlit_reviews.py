60fd4be274edf51b9c2de2ca47e84a2296f4251f
Karthikn96 <82644968+Karthikn96@users.noreply.github.com>
2021-04-18 05:01:52 +0530

    Update Streamlit_reviews.py

--- Streamlit_reviews.py
+++ Streamlit_reviews.py
@@ -22,7 +22,10 @@ from gensim.test.utils import common_texts
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import wordnet
-import streamlit.components.v1 as stc
+nltk.download('stopwords')
+nltk.download('vader_lexicon')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 
 
 st.title("Question 1 Part 2")
